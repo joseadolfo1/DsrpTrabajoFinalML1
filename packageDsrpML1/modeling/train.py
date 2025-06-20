aaa3043d1438b462ca2645c7c940c1addc699d5b
joseadolfo1 <joseadolfo.c.tmd@gmail.com>
2025-06-20 15:43:59 -0500

    Desarrollo Modelo Regresor Logistico

--- packageDsrpML1/modeling/train.py
+++ packageDsrpML1/modeling/train.py
@@ -1,30 +1,59 @@
-from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, roc_auc_score
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix
 
-from loguru import logger
-from tqdm import tqdm
-import typer
+class MLProc:
 
-from packageDsrpML1.config import MODELS_DIR, PROCESSED_DATA_DIR
+    def __init__(self, model_name:str, data):
+        self.model_name = model_name
+        self.data = data
+      
+    def pre_process_data(self):
+        self.data["is_genuine"] = self.data["is_genuine"].astype(int)
+        self.data.dropna(inplace=True)
+        print("Vistazo data depurada:")
+        print("----------------------")
+        print(self.data.info())
+        print("\n\n")
+        
+    def split_data(self):
+        self.pre_process_data()
+        x = self.data.drop("is_genuine", axis=1)
+        y = self.data["is_genuine"]
+        print("1.Separando datos de train y test...")
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, random_state=100, test_size=0.2)
+        
+    def train(self, model):        
+        self.split_data()
+        print(f"2.Entrenando model {self.model_name}")
+        self.fitted_model = model.fit(self.x_train, self.y_train)
+        print("3.Entrenamiento Finalizado....")
 
-app = typer.Typer()
+    def predict(self):
+        self.predictions = self.fitted_model.predict(self.x_test)
 
+    def metricas(self):
+        print("*********************************************************************")
+        print(f"Mostrando Metricas pre-establecidas del modelo {self.model_name}")
+        print("*********************************************************************")
+        nombre = type(self.fitted_model).__name__
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+        if nombre == "LogisticRegression":
+            #print(self.predictions)
+            print(classification_report(self.predictions, self.y_test))
+            cm = confusion_matrix(self.y_test, self.predictions)
+            disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+            disp.plot()
+            disp.ax_.set_title("Matriz de Confusión: Logistic Regression")
+        elif nombre == "LinearRegression":
+            print(nombre)
+        elif nombre == "PCA":
+            print(nombre)
+        elif model_name == "KMeans":
+             print(nombre)
+        else:
+            print("Modelo no reconocido.")
+        
 
-
-if __name__ == "__main__":
-    app()
+    
