cd4a2460b5c88502fa182fff2116eeb832cadaf5
joseadolfo1 <joseadolfo.c.tmd@gmail.com>
2025-06-21 15:09:31 -0500

    agregando variables de directorios de reportes y modelos

--- packageDsrpML1/config.py
+++ packageDsrpML1/config.py
@@ -7,9 +7,9 @@ PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"La ruta del proyecto es: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
-CSV_FILE = DATA_DIR / "fake_bills.csv" 
-
+CSV_FILE = DATA_DIR / "fake_bills.csv"
 MODELS_DIR = PROJ_ROOT / "models"
+REPORTS_DIR = PROJ_ROOT / "reports"
 
 
 
